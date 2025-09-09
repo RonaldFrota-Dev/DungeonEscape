4d8fd4fd19f0bfd00d9c894e20c499cd4f48c317
Ronald Frota <cgscroll14@gmail.com>
2025-09-09 14:18:51 -0300

    feat(door): add a pressure plate trigger using a cpp box component trigger

---
+++ Source/DungeonEscape/TriggerComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "MoverComponent.h"
+#include "TriggerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUNGEONESCAPE_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTriggerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	AActor* MoverActor;
+
+	UMoverComponent* Mover;
+
+	UPROPERTY(EditAnywhere)
+	bool IsPressurePlate = false;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
